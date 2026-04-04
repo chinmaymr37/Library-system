43cfc3f84fe5148047866cf5c6c184673193ab2d
Amoghmr123 <amoghmr2004@gmail.com>
2026-04-04 16:32:54 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,231 @@
+from flask import Flask, render_template, request, redirect, flash , session, url_for
+from models.database import create_table, add_book ,add_copy , get_all_books , delete_book ,issue_book , return_book, get_copy_by_barcode, get_overdue_books, get_user_by_username , add_member , get_all_members, get_issued_books , get_member_issues , add_user , search_books
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def login_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if "username" not in session:
+            return redirect(url_for("login"))
+        return f(*args, **kwargs)
+    return wrapper
+
+create_table()
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route("/")
+def home():
+    rows = get_all_books()
+    total = sum(row[4] for row in rows)
+    available = sum(row[5] for row in rows)
+    issued = total - available
+
+    return render_template("home.html", total=total, available=available, issued=issued)
+
+@app.route("/add_book", methods=["GET", "POST"])
+@login_required
+def add_book_route():
+    if session.get("role") != "admin":
+        flash("Only admin can add books")
+        return redirect(url_for("home"))
+    action = request.form.get("action")
+    if action == "add_book":
+        if request.method == "POST":
+            ISBN = request.form["ISBN"]
+            title = request.form["title"]
+            author = request.form["author"]
+            category = request.form["category"]
+            rack = request.form["rack_number"]
+            shelf = request.form["shelf_number"]
+            barcode = request.form["barcode"]
+
+            book_id, result = add_book(ISBN, title, author, category, rack, shelf)
+            if result:
+                add_copy(book_id, barcode)
+                flash("Book added successfully")
+            else:
+                flash("Book with same ISBN already exists")
+            
+    elif action == "add_copy":
+        if request.method == "POST":
+            book_id = int(request.form["book_id"])
+            barcode = request.form["barcode"]
+
+            add_copy(book_id, barcode)
+
+    return render_template("add_book.html")
+
+@app.route("/add_member", methods=["GET", "POST"])
+@login_required
+def add_member_route():
+    if session.get("role") != "admin":
+        flash("Only admin can add members")
+        return redirect(url_for("home"))
+    if request.method == "POST":
+        name = request.form["name"]
+        class_ = request.form["class"]
+        section = request.form["section"]
+        roll_number = int(request.form["roll_number"])
+
+        result = add_member(name, class_, section, roll_number)
+        if result:
+            flash("Member added successfully")
+        else:
+            flash("Member with same roll number already exists")
+
+    return render_template("add_member.html")
+
+@app.route("/issue_book", methods=["GET", "POST"])
+@login_required
+def issue_book_route():
+    if session.get("role") != "admin":
+        flash("Only admin can issue books")
+        return redirect(url_for("home"))
+    if request.method == "POST":
+        barcode = request.form["barcode"]
+        member_id = int(request.form["member_id"])
+        duration = int(request.form["duration"])
+        result = issue_book(barcode, member_id, duration)
+
+        if result:
+            flash("Book issued successfully")
+        else:
+            flash("Book not available or invalid barcode")
+
+        return redirect("/issue_book")
+
+    return render_template("issue_book.html")
+
+@app.route("/return_book", methods=["GET", "POST"])
+@login_required
+def return_book_route():
+    if session.get("role") != "admin":
+        flash("Only admin can return books")
+        return redirect(url_for("home"))
+    if request.method == "POST":
+        barcode = request.form["barcode"]
+        result, fine = return_book(barcode)
+
+        if result:
+            flash(f"Book returned successfully. Fine: ₹{fine}")
+        else:
+            flash("Invalid barcode or book already returned")
+
+        return redirect("/return_book")
+
+    return render_template("return_book.html")
+
+@app.route("/view_books")
+@login_required
+def view_books_route():
+    rows = get_all_books()
+    filter_type = request.args.get("filter")
+    if filter_type == "available":
+        rows = [row for row in rows if row[5] >= 1]
+    elif filter_type == "issued":
+        rows = [row for row in rows if row[5] == 0]
+    return render_template("view_books.html", rows=rows)
+
+@app.route("/view_members")
+@login_required
+def view_members_route():
+    rows = get_all_members()
+    return render_template("view_members.html", rows=rows)
+
+@app.route("/delete/<int:book_id>")
+@login_required
+def delete(book_id):
+    if session.get("role") != "admin":
+        flash("Only admin can delete books")
+        return redirect(url_for("home"))
+    result = delete_book(book_id)
+    if result:
+        flash("Book deleted successfully")
+    else:
+        flash("Book cannot be deleted as it is currently issued")
+    return redirect("/view_books")
+
+@app.route("/search", methods=["GET", "POST"])
+@login_required
+def search():
+    copy = None
+
+    if request.method == "POST":
+        input_value = request.form["input_value"]
+        copy, result = search_books(input_value)
+        if not result:
+            flash("No book found with the given input")
+
+    return render_template("search.html", copy=copy)
+
+@app.route("/overdue")
+@login_required
+def overdue():
+    rows = get_overdue_books()
+    return render_template("overdue.html", rows=rows)
+
+@app.route("/issued_books")
+@login_required
+def issued_books():
+    rows = get_issued_books()
+    return render_template("issued_books.html", rows=rows)
+
+@app.route("/member_dashboard")
+@login_required
+def member_dashboard():
+    rows = get_member_issues(session.get("member_id"))
+    return render_template("member_dashboard.html", rows=rows)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = get_user_by_username(username)
+        if user is not None :
+            session["username"] = username
+            session["role"] = user[4]
+            session["member_id"] = user[1]
+            flash("Login successful")
+            return redirect(url_for("home"))
+        else:
+            flash("wrong username OR password")
+            flash("Login failed")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    session.pop("username", None)
+    session.pop("role", None)
+    session.pop("member_id", None)
+    flash("Logged out successfully")
+    return redirect(url_for("login"))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if session.get("role") != "admin":
+        flash("Only admin can register users")
+        return redirect(url_for("home"))
+    if request.method == "POST":
+        member_id = int(request.form["member_id"])
+        username = request.form["username"]
+        password = request.form["password"]
+        password = generate_password_hash(password)
+        role = request.form["role"]
+        add_user(member_id, username, password, role)
+
+        flash("User registered successfully")
+        return redirect(url_for("register"))
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
---
+++ models/database.py
@@ -0,0 +1,440 @@
+import sqlite3
+
+def create_table():
+    create_book_table()
+    create_member_table()
+    create_issue_table() 
+    create_user_table()      
+    create_copies_table() 
+
+def create_book_table():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS books (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ISBN TEXT UNIQUE,
+        title TEXT NOT NULL,
+        author TEXT NOT NULL,
+        category TEXT NOT NULL,
+        rack TEXT,
+        shelf TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def create_copies_table():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS copies (
+        copy_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        book_id INTEGER NOT NULL,
+        barcode TEXT UNIQUE NOT NULL,
+        available INTEGER NOT NULL,
+        FOREIGN KEY (book_id) REFERENCES books(id)
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def create_member_table():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS members (
+        member_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        class INTEGER ,
+        section TEXT ,
+        roll_number INTEGER NOT NULL UNIQUE
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def create_issue_table():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS issues (
+        issue_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        member_id INTEGER NOT NULL,
+        copy_id INTEGER NOT NULL,
+        issue_date TEXT NOT NULL,
+        due_date TEXT NOT NULL,
+        return_date TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def create_user_table():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        member_id INTEGER ,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        role TEXT NOT NULL,
+        last_login TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def add_book(ISBN, title, author, category, rack, shelf):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT * FROM books WHERE ISBN = ?", (ISBN,))
+    existing_book = cursor.fetchone()
+    if existing_book:
+        conn.close()
+        return None, False
+
+    cursor.execute("""
+    INSERT INTO books (ISBN, title, author, category, rack, shelf)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """, (ISBN, title, author, category, rack, shelf))
+
+    book_id = cursor.lastrowid
+
+    conn.commit()
+    conn.close()
+    return book_id , True
+
+def add_copy(book_id, barcode):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO copies (book_id, barcode, available)
+    VALUES (?, ?, 1)
+    """, (book_id, barcode))
+
+    conn.commit()
+    conn.close()
+
+def get_all_books():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT 
+        books.id,
+        books.title,
+        books.author,
+        books.category,
+        COUNT(copies.copy_id) AS total,
+        COALESCE(SUM(copies.available), 0) AS available
+    FROM books
+    LEFT JOIN copies ON books.id = copies.book_id
+    GROUP BY books.id
+    """)
+    books = cursor.fetchall()
+
+    conn.close()
+    return books
+
+def delete_book(book_id):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT COUNT(*) FROM copies 
+    WHERE book_id = ? AND available = 0
+    """, (book_id,))
+    
+    issued_count = cursor.fetchone()[0]
+
+    if issued_count > 0:
+        conn.close()
+        return False
+
+    cursor.execute("DELETE FROM copies WHERE book_id = ?", (book_id,))
+    cursor.execute("DELETE FROM books WHERE id = ?", (book_id,))
+
+    conn.commit()
+    conn.close()
+
+    return True
+
+def issue_book(barcode ,member_id , duration):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    
+    copy = get_copy_by_barcode(barcode)
+    member = get_member_by_member_id(member_id)
+
+    if copy is None:
+        conn.close()
+        return False
+
+    if copy[3] == 0:
+        conn.close()
+        return False
+
+    if member is None:
+        conn.close()
+        return False
+
+
+    from datetime import date, timedelta
+                
+    issue_date = date.today()
+    due_date = issue_date + timedelta(days=duration)
+
+    cursor.execute(
+    "INSERT INTO issues (member_id, copy_id, issue_date, due_date ,return_date) VALUES (?, ?, ?, ?, NULL)",
+    (member_id, copy[0], issue_date, due_date)
+    )
+
+    cursor.execute(
+    "UPDATE copies SET available = 0 WHERE copy_id = ? AND available = 1",
+    (copy[0],)
+    )
+
+    conn.commit()
+    updated = cursor.rowcount
+    conn.close()
+
+    return updated > 0
+
+def return_book(barcode):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    copy = get_copy_by_barcode(barcode)
+    if copy is None:
+        conn.close()
+        return False, 0
+    if copy[3] == 1:
+        conn.close()
+        return False, 0
+    
+    copy_id = copy[0]
+    from datetime import date
+    return_date = date.today()
+
+    cursor.execute(
+        "SELECT * FROM issues WHERE copy_id = ? AND return_date IS NULL",
+        (copy_id,)
+    )
+    issue = cursor.fetchone()
+    if issue is None:
+        conn.close()
+        return False, 0
+    rate = 10 
+    due_date = date.fromisoformat(issue[4])
+    days_late = max(0, (return_date - due_date).days)
+    fine = days_late * rate
+
+    cursor.execute(
+        "UPDATE issues SET return_date = ? WHERE copy_id = ? AND return_date IS NULL",
+        (return_date, copy_id)
+    )
+
+    updated = cursor.rowcount
+    if updated == 0:
+        conn.close()
+        return False, 0
+        
+    cursor.execute(
+        "UPDATE copies SET available = 1 WHERE copy_id = ? AND available = 0",
+        (copy_id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return True, fine
+
+def get_copy_by_barcode(barcode):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM copies WHERE barcode = ?", (barcode,))
+    book = cursor.fetchone()
+
+    conn.close()
+    return book
+
+def get_member_by_member_id(member_id):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM members WHERE member_id = ?", (member_id,))
+    member = cursor.fetchone()
+
+    conn.close()
+    return member
+    
+def get_overdue_books():
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    from datetime import date
+    today = date.today()
+
+    cursor.execute("""
+    SELECT * FROM issues WHERE return_date IS NULL AND due_date < ?
+    """, (today,))
+    overdue= cursor.fetchall()
+    result = []
+    for row in overdue:
+        due_date = date.fromisoformat(row[4])
+        days_late = max(0, (today - due_date).days)
+        rate = 10
+        fine = days_late * rate
+        result.append((row[0], row[1], row[2], row[3], row[4], fine))
+    conn.close()
+    return result
+
+def get_user_by_username(username):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT * FROM users WHERE username = ?
+    """, (username,)
+    )
+    user = cursor.fetchone()
+    if user is not None:
+        conn.close()
+        return user
+
+    conn.close()
+    return user
+
+def add_member(name, class_, section, roll_number):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM members WHERE roll_number = ?", (roll_number,))
+    existing_member = cursor.fetchone()
+    if existing_member:
+        conn.close()
+        return False
+
+    cursor.execute("""
+    INSERT INTO members (name, class, section, roll_number)
+    VALUES (?, ?, ?, ?)
+    """, (name, class_, section, roll_number))
+
+    conn.commit()
+    conn.close()
+
+    return True
+
+def get_all_members():
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM members")
+    rows = cursor.fetchall()
+
+    conn.close()
+    return rows
+
+def get_issued_books():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT m.name, b.title, c.barcode, i.issue_date, i.due_date, i.return_date
+    FROM issues i
+    JOIN members m ON i.member_id = m.member_id
+    JOIN copies c ON i.copy_id = c.copy_id
+    JOIN books b ON c.book_id = b.id
+    """)
+
+    rows = cursor.fetchall()
+
+    conn.close()
+    return rows
+
+def get_member_issues(member_id):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    from datetime import date
+    today = date.today()
+
+    cursor.execute("""
+    SELECT b.title, i.issue_date, i.due_date, i.return_date 
+    FROM issues i
+    JOIN copies c ON i.copy_id = c.copy_id
+    JOIN books b ON c.book_id = b.id
+    WHERE i.member_id = ?
+    """, (member_id,))
+
+    rows = cursor.fetchall()
+    result = []
+    for row in rows:
+        due_date = date.fromisoformat(row[2])
+        days_late = max(0, (today - due_date).days)
+        rate = 10
+        fine = days_late * rate
+        is_overdue = row[3] is None and today > due_date
+        result.append((row[0], row[1], row[2], row[3], fine, is_overdue))
+
+    conn.close()
+    return result
+
+def add_user(member_id, username, password, role):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO users (member_id, username, password, role)
+    VALUES (?, ?, ?, ?)
+    """, (member_id, username, password, role))
+
+    conn.commit()
+    conn.close()
+
+def search_books(input_value):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    base_query = """
+    SELECT b.id, b.title, b.author, b.category,
+           COUNT(c.copy_id) AS total_copies,
+           COALESCE(SUM(c.available), 0) AS available_copies
+    FROM books b
+    LEFT JOIN copies c ON b.id = c.book_id
+    """
+
+    if input_value.isdigit():
+        query = base_query + " WHERE b.id = ? GROUP BY b.id"
+        cursor.execute(query, (input_value,))
+    else:
+        query = base_query + " WHERE b.title LIKE ? GROUP BY b.id"
+        cursor.execute(query, (f"%{input_value}%",))
+
+    results = cursor.fetchall()
+    conn.close()
+    return results, bool(results)
+
+
